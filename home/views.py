874625c8d4d604df622f662a73eadae0104ddce9
Qingying Meng <qmeng222@gmail.com>
2022-08-09 12:05:16 -0700

    LUD: 8/9 12pm

--- home/views.py
+++ home/views.py
@@ -3,20 +3,26 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return  HttpResponse('This is the home page ()')
+    # return  HttpResponse('This is the home page ()')
+    context = {'name': 'Qingying', 'course': 'Django'}
+    return render(request, 'home.html', context)
 
 
 def about(request):
-    return  HttpResponse('This is the about page (about/)')
+    # return  HttpResponse('This is the about page (about/)')
+    return render(request, 'about.html')
 
 
 def projects(request):
-    return  HttpResponse('This is the projects page (projects/)')
+    # return  HttpResponse('This is the projects page (projects/)')
+    return render(request, 'projects.html')
 
 
 def skills(request):
-    return  HttpResponse('This is the skills page (skills/)')
+    # return  HttpResponse('This is the skills page (skills/)')
+    return render(request, 'skills.html')
 
 
 def contact(request):
-    return  HttpResponse('This is the contact page (contact/)')
+    # return  HttpResponse('This is the contact page (contact/)')
+    return render(request, 'contact.html')
